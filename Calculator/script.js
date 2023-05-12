2713e0f9b60d52c1c651931ae53b968843a06ba3
Om Rawaley <133281331+omrawaley@users.noreply.github.com>
2023-05-11 21:31:46 -0400

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,39 @@
+function calculateCylinder()
+{
+	var cylinderR = document.getElementById("cylinderR").value;
+	var cylinderH = document.getElementById("cylinderH").value;
+
+	var cylinderVolume = cylinderR * cylinderR * cylinderH * 3.14;
+	var cylinderSurfaceArea = 2 * 3.14 * cylinderR * cylinderR + 2 * 3.14 * cylinderR * cylinderH;
+
+	document.getElementById("cylinderVOut").innerHTML = "Volume: " + cylinderVolume;
+	document.getElementById("cylinderSAOut").innerHTML = "Surface Area: " + cylinderSurfaceArea;
+}
+
+function calculateTriangularPrism()
+{
+	var triangularPrismA = document.getElementById("triangularPrismA").value;
+	var triangularPrismB = document.getElementById("triangularPrismB").value;
+	var triangularPrismC = document.getElementById("triangularPrismC").value;
+	var triangularPrismH = document.getElementById("triangularPrismH").value;
+	var triangularPrismL = document.getElementById("triangularPrismL").value;
+
+	var triangularPrismVolume = (triangularPrismB * triangularPrismH * triangularPrismL) / 2;
+	var triangularPrismSurfaceArea = triangularPrismA * triangularPrismL + triangularPrismB * triangularPrismL + triangularPrismC * triangularPrismL + triangularPrismB * triangularPrismH;
+
+	document.getElementById("triangularPrismVOut").innerHTML = "Volume: " + triangularPrismVolume;
+	document.getElementById("triangularPrismSAOut").innerHTML = "Surface Area: " + triangularPrismSurfaceArea;
+}
+
+function calculateRectangularPrism()
+{
+	var rectangularPrismW = document.getElementById("rectangularPrismW").value;
+	var rectangularPrismH = document.getElementById("rectangularPrismH").value;
+	var rectangularPrismL = document.getElementById("rectangularPrismL").value;
+
+	var rectangularPrismVolume = rectangularPrismW * rectangularPrismH * rectangularPrismL;
+	var rectangularPrismSurfaceArea = 2 * (rectangularPrismW * rectangularPrismL + rectangularPrismH * rectangularPrismL + rectangularPrismW * rectangularPrismH);
+
+	document.getElementById("rectangularPrismVOut").innerHTML = "Volume: " + rectangularPrismVolume;
+	document.getElementById("rectangularPrismSAOut").innerHTML = "Surface Area: " + rectangularPrismSurfaceArea;
+}
